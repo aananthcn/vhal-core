b75fb9c8a259900ec246e15771b2bf959b7a71f7
Aananth C N <c.n.aananth@gmail.com>
2026-05-04 15:34:27 +0530

    Renamed all occurences of RPi5 to RPi
    As RPi4B is to be used and the same software is working, renaming of
    file, variables and functions becomes important to keep the clarity.

--- packages/vhal-types/stubs/utils/Trace.h
+++ packages/vhal-types/stubs/utils/Trace.h
@@ -3,7 +3,9 @@
 
 // All trace macros are no-ops on Linux
 #define ATRACE_TAG_ALWAYS   (1 << 31)
+#ifndef ATRACE_TAG
 #define ATRACE_TAG          ATRACE_TAG_ALWAYS
+#endif
 #define ATRACE_CALL()
 #define ATRACE_NAME(name)
 #define ATRACE_INT(name, val)
